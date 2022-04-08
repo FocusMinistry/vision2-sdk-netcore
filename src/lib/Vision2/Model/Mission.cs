895649b1aa24bfb6e411d71d1b9ae7c75ce7dade
Chad <chadmeyer@me.com>
2022-04-07 20:31:11 -0500

    nullable changes to mission

--- src/lib/Vision2/Model/Mission.cs
+++ src/lib/Vision2/Model/Mission.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Vision2.Model {
-    public class Mission { 
+    public class Mission {
         public Mission() {
 
         }
@@ -15,14 +15,15 @@ namespace Vision2.Model {
         public decimal TargetPerPartcipant { get; set; }
         public int NameDisplay { get; set; }
         public int NumberOfActiveParticipants { get; set; }
-        public int FundraisingCampaignId { get; set; }
+        public int? FundraisingCampaignId { get; set; }
         public string FundraisingCampaignName { get; set; }
-        public int VolunteerCampaignId { get; set; }
+        public int? VolunteerCampaignId { get; set; }
         public string VolunteerCampaignName { get; set; }
         public string BasketCategory { get; set; }
         public string Tags { get; set; }
         public string SortKey { get; set; }
         public bool IsFundraiserContentApprovalRequired { get; set; }
+        public bool IsStatusNotChangedBasedOnStartDate { get; set; }
         public bool IsShowOnlineIfGoalMet { get; set; }
         public string Name { get; set; }
         public Enum.ProjectStatusType Status { get; set; }
@@ -42,25 +43,25 @@ namespace Vision2.Model {
         public bool EnableRecurringGiftPaymentFailure { get; set; }
         public bool EnableScheduledPaymentNotifications { get; set; }
         public string AccountSegment { get; set; }
-        public int DefaultGivingLinkId { get; set; }
+        public int? DefaultGivingLinkId { get; set; }
         public string DefaultGivingLinkName { get; set; }
-        public int DefaultConnectLinkId { get; set; }
+        public int? DefaultConnectLinkId { get; set; }
         public string DefaultConnectLinkName { get; set; }
         public int? DefaultCommitmentLinkId { get; set; }
         public string DefaultCommitmentLinkName { get; set; }
         public bool EnableFirstGiftNotifications { get; set; }
         public bool EnableLastGiftNotifications { get; set; }
-        public int ExpiringRecurringGiftTriggerId { get; set; }
-        public int RecurringGiftPaymentFailureTriggerId { get; set; }
-        public int ScheduledCommitmentPaymentTriggerId { get; set; }
-        public int FirstGiftTriggerId { get; set; }
-        public int LastGiftTriggerId { get; set; }
+        public int? ExpiringRecurringGiftTriggerId { get; set; }
+        public int? RecurringGiftPaymentFailureTriggerId { get; set; }
+        public int? ScheduledCommitmentPaymentTriggerId { get; set; }
+        public int? FirstGiftTriggerId { get; set; }
+        public int? LastGiftTriggerId { get; set; }
         public int? DepartmentId { get; set; }
         public decimal GoalAmount { get; set; }
         public bool IncludePledgesInGoalProgress { get; set; }
         public bool IsArchived { get; set; }
         public bool IsTithe { get; set; }
-        public int NonCharitableOrganizationFundId { get; set; }
+        public int? NonCharitableOrganizationFundId { get; set; }
         public bool IsCharitable { get; set; }
         public bool IsDefaultForType { get; set; }
     }
